6364d4120f783f4fdad01a7e46def8476d612c77
hjkrannig <hj@krannig.com>
2019-02-24 19:42:18 +0100

    Comment-Model und komplette Logik implementiert...

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required, permission_required
 
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
@@ -60,11 +60,38 @@ def post_publish(request, pk):
     return redirect('post_detail', pk=pk)
 
 @login_required
-@permission_required('post.can_delete', raise_exception=True)
+@permission_required('blog.delete_post', raise_exception=True)
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
 
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+@permission_required('blog.delete_comment', raise_exception=True)
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+@permission_required('blog.delete_comment', raise_exception=True)
+def comment_remove(request, pk):
+    pass
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
 
 # ...ab hier Helferfunktionen
