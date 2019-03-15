5cf2118e6980257949cc5247c88f3fd51f356d99
hjkrannig <hj@krannig.com>
2019-03-15 12:45:05 +0100

    Listendarstellung und Links zu post_details.html überarbeitet...

--- blog/views.py
+++ blog/views.py
@@ -77,7 +77,7 @@ def add_comment_to_post(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
-    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+    return render(request, 'blog/add_comment_to_post.html', {'form': form, 'post': post})
 
 @login_required
 @permission_required('blog.delete_comment', raise_exception=True)
